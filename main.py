06636b76cd300a4f7e246184107a69ea7981e3c0
190BJK1903 <143109080+190BJK1903@users.noreply.github.com>
2023-10-26 21:08:13 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#siniflar class
+
+class Hasta():
+    def __init__(self, name, problem, yas):
+        self.name = name
+        self.problem = problem
+        self.yas = yas
+        self.liste = []
+
+def problem_ekle(self, problem):
+    
+
+
+
+hasta = Hasta("Ricardo Quaresma", "ayak kirigi", 38)   #
+hasta2 = Hasta("Anderson Talisca", "bas agrisi", 30)    
+
+
+print(hasta.name, hasta.problem, hasta.yas)
+print(hasta2.problem)
+
+
+
